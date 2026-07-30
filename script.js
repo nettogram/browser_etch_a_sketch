b8683ec9f81b24a44ee4cdba9859366a71c3e9ee
NJ <niljohna77@gmail.com>
2026-07-30 14:52:15 -0700

    Force canvas to be a square

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const gridCount = 16 * 16;
 
 for (let i = 0; i < gridCount; i++) {
     const eccSquare = document.createElement("div");
-    eccSquare.setAttribute("style", "background-color: red; border: 2px solid black; height: 30px; width: 30px;")
+    eccSquare.setAttribute("style", "background-color: red; border: 2px solid black; height: 20px; width: 20px;")
     eccSquare.textContent = " ";
     eccContainer.appendChild(eccSquare);
 }

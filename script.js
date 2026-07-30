f64f4c5ff92b8a8b1da023ae4bbf4a2d934efa46
NJ <niljohna77@gmail.com>
2026-07-30 15:09:32 -0700

    Fix box sizing, add draw functionality
    -Draw functionality achieved by adding event
    handler that knows when mouse hovers over a div
    and changes color promptly

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const gridCount = 16 * 16;
 
 for (let i = 0; i < gridCount; i++) {
     const eccSquare = document.createElement("div");
-    eccSquare.setAttribute("style", "background-color: red; border: 2px solid black; height: 20px; width: 20px;")
+    eccSquare.setAttribute("style", "height: 20px; width: 20px;");
     eccSquare.textContent = " ";
     eccContainer.appendChild(eccSquare);
+    eccSquare.addEventListener("mouseover", (event) => eccSquare.style.backgroundColor = "black")
 }

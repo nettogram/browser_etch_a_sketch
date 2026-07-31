37ec782381f69871877dfb2e95eee14514bbdefe
NJ <niljohna77@gmail.com>
2026-07-30 19:34:00 -0700

    Complete full first iteration
    -Addition of new functions/refactorizations:
    setGridCount()
    setCanvasSize()
    removeCanvas() - operates on NodeList
    changeResolution() - for button event listener

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 const eccContainer = document.querySelector(".canvas");
-let boxCount = 16;
-const gridCount = boxCount * boxCount;
-eccContainer.style.height = boxCount * 20;
+const changeResolutionButton = document.querySelector("button");
 
-for (let i = 0; i < gridCount; i++) {
-    const eccSquare = document.createElement("div");
-    eccSquare.style.height = "20px";
-    eccSquare.style.width = "20px";
-    eccSquare.textContent = " ";
-    eccContainer.appendChild(eccSquare);
-    eccSquare.addEventListener("mouseover", (event) => eccSquare.style.backgroundColor = "black")
+let boxCount = 16;
+let gridCount = boxCount * boxCount;
+
+function setGridCount() {
+    gridCount = boxCount * boxCount;
+
 }
 
+function setCanvasSize() {
+    eccContainer.style.height = `${boxCount * 20}px`;
+    eccContainer.style.width = `${boxCount * 20}px`;
+}
+
+function createCanvas() {
+    for (let i = 0; i < gridCount; i++) {
+        const eccSquare = document.createElement("div");
+        eccSquare.style.height = "20px";
+        eccSquare.style.width = "20px";
+        eccSquare.textContent = " ";
+        eccSquare.classList.add("canvas-pixels")
+        eccContainer.appendChild(eccSquare);
+        eccSquare.addEventListener("mouseover", (event) => eccSquare.style.backgroundColor = "black");
+        console.log(document.querySelectorAll(".canvas-pixels").length);
+    }
+}
+
+function removeCanvas() {
+    let currentResolution = document.querySelectorAll(".canvas-pixels");
+    for (let i = 0; i < gridCount; i++)
+    currentResolution[i].remove();
+}
+
+function changeResolution() {
+    let resolution = prompt("Enter a new resolution between 0 and 50");
+    if (resolution > 0 && resolution <= 50) {
+        removeCanvas();
+        boxCount = resolution;
+        setGridCount();
+        setCanvasSize();
+        createCanvas();
+    }
+
+}
+
+createCanvas();
+changeResolutionButton.addEventListener("click", changeResolution);
